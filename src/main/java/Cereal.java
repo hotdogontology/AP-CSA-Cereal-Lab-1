93515a7b088c01c500a028d1219251908a2544e5
Taylor Belcher <30426680+hotdogontology@users.noreply.github.com>
2026-03-05 12:20:11 -0500

    Add files via upload

---
+++ src/main/java/Cereal.java
@@ -0,0 +1,19 @@
+/**
+ * Cereal.java
+ *
+ * @author – Your name
+ * @author – Class period
+ *
+ */
+public class Cereal
+{
+    // private instance variables to hold info about a cereal from the data set
+
+    // constructor that creates a cereal object
+
+    // accessor methods for each instance variable
+
+    /* toString method returns the values stored in the instance variables
+       concatenated in a sentence starting with "Cereal: "
+
+}
